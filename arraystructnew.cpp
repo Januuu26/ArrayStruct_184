f5e343d47c3c189859da46662936559fbe7e8f70
Januuu26 <b77286116@gmail.com>
2026-04-06 15:35:56 +0700

    mengubah menjadi 4

--- arraystructnew.cpp
+++ arraystructnew.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-    int aku[5] = {10, 20, 30, 40, 50};
+    int aku[4] = {10, 20, 30, 40};
 
-    int kamu[5];
+    int kamu[4];
 
     cout << "Data pada index 1 = " << aku[1] << endl;
     
