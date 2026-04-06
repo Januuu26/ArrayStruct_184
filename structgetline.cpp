57fb61a5e6988f126e508ca329d027e3ea45858e
Januuu26 <b77286116@gmail.com>
2026-04-06 15:58:35 +0700

    menambah getline

---
+++ structgetline.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct detailAlamat
+{
+    string kota;
+    string desa;
+};
+
+struct orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main()
+{
+    orang mhs;
+    cout << "Nama : ";
+    getline(cin, mhs.nama);
+    cout << "Alamat desa : ";
+    getline(cin, mhs.alamat.desa);
+    cout << "Alamat kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat.desa << endl;
+    cout << "Alamat : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
+
