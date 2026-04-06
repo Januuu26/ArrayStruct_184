9d15a6a4e7dd8d9cb14b0b3cabe5f61f68be970e
Januuu26 <b77286116@gmail.com>
2026-04-06 15:54:35 +0700

    membuatt output

--- structNew2.cpp
+++ structNew2.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 struct detailAlamat
 {
-    string nama;
-    string alamat;
-    int umur;
+    string kota;
+    string desa;
 };
 
 struct orang
@@ -15,3 +14,22 @@ struct orang
     detailAlamat alamat;
     int umur;
 };
+
+int main()
+{
+    orang mhs;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Alamat kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat.desa << endl;
+    cout << "Alamat : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
