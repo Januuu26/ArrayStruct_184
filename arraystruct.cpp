8a6831eda1c90ed2a294e19474d597ff9be379d5
Januuu26 <b77286116@gmail.com>
2026-04-06 16:01:45 +0700

    membuat for

--- arraystruct.cpp
+++ arraystruct.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
-    int aku[5] = {5, 10, 15, 20, 25};
+struct detailAlamat
+{
+    string kota;
+    string desa;
+};
 
-    int kamu [5];
+struct orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main()
+{
+    orang mhs;
+    for (int i = 0; i < 3; i++)
+    {
+    cout << "Data ke-" << i+1 << endl;
+    cout << "Nama : ";
+    getline(cin, mhs.nama);
+    cout << "Alamat desa : ";
+    getline(cin, mhs.alamat.desa);
+    cout << "Alamat kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {       
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat.desa << endl;
+    cout << "Alamat : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+    }
 }
