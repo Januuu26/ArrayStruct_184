34c9e8c84cada73e85c2f89fab413d176c486b61
Januuu26 <b77286116@gmail.com>
2026-04-06 15:12:02 +0700

    membuat ouput data

--- arraystructnew.cpp
+++ arraystructnew.cpp
@@ -4,5 +4,12 @@ using namespace std;
 int main() {
     int aku[5] = {1, 2, 3, 4, 5};
 
+    int kamu[5];
+
+    cout << "Data pada index 1 = " << aku[1] << endl;
     
+    aku[1] = 10;
+    cout << endl;
+    cout << "Data pada index 1 = " << aku[1] << endl;
+
 }
